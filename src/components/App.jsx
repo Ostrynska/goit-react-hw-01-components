af446d3b355aaa6e654b8cdc5b5bcdb1dc3dc129
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-15 22:47:44 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import Profile from './Profile'
-import user from '../data/user';
-import Statistics from './Statistics';
+import user from '../data/user'
+import Statistics from './Statistics'
 import data from '../data/data'
+import FriendList from './FriendList'
+import friends from '../data/friends'
+import TransactionHistory from './TransactionHistory'
+import transactions from '../data/transactions'
 
 const App = () =>
 {
@@ -19,6 +23,12 @@ const App = () =>
       <Statistics 
         title="Upload stats"  
         stats={data}
+      />
+      <FriendList 
+        friends={friends}
+      />
+      <TransactionHistory 
+        items={transactions}
       />
     </div>
   );
