fe5177f25ed49b119826d2bf25be0a699ea5a90a
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-16 22:59:10 +0200

    Update FriendList.jsx

--- src/components/03-friendList/FriendList.jsx
+++ src/components/03-friendList/FriendList.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function FriendList({ friends }) {
     return (
-        <ul class="friend-list">
+        <ul className="friend-list">
             {friends.map(friend => (
                 <Friend
                     key={friend.id}
