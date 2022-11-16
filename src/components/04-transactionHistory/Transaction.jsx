3d11f655c06d232308dcd5f4c8c98ac5c30a9890
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-16 13:45:49 +0200

    Create Transaction.jsx

---
+++ src/components/04-transactionHistory/Transaction.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+const Transaction = ({
+id, type, amount, currency }) =>
+{
+    return (
+        <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+        </tr>
+    )
+}
+
+Transaction.propTypes = {
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+}
+
+export default Transaction;
