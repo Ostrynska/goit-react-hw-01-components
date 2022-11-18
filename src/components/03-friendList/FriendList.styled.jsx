f224244ac9ea6dc1d1dae5f9239b52539e7fca65
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-19 00:06:17 +0200

    Create FriendList.styled.jsx

---
+++ src/components/03-friendList/FriendList.styled.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Friends = styled.ul`
+    margin: auto;
+    margin-top: ${p => p.theme.sizes.xs};
+    width: ${p => p.theme.sizes.s};
+`
+
+export const Friend = styled.li`
+    margin-bottom: 5px;
+    display: ${p => p.theme.display.inlineFlex};
+    align-items: center;
+    justify-content: flex-start;
+    gap: ${p => p.theme.space[4]}px;
+    background-color: ${p => p.theme.colors.primary};
+    width: ${p => p.theme.sizes.s};
+    height: 64px;
+    box-shadow: ${p => p.theme.shadows.accent};
+    border-radius: ${p => p.theme.radii.normal};
+    transition: ${p => p.theme.transition};
+
+    :hover,
+    :focus {
+    background-color: ${p => p.theme.colors.accent};
+    color:  ${p => p.theme.colors.white};
+    transform: scale(1.05);
+    transition: transform 0.3s ease-in-out 0s;
+`
+export const FriendAvatar = styled.img`
+`
+export const FriendStatus = styled.span`
+    width: 10px;
+    height: 10px;
+    margin-left: 10px;
+    border-radius: ${p => p.theme.radii.round};
+    background-color: ${p => p.children ? p.theme.colors.background : p.theme.colors.secondary 
+    }
+`
+export const FriendName = styled.p`
+    margin-left: 12px;
+`
