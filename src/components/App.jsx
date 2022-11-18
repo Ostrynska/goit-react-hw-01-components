0d9382759605e9f8d25074fe747c798e03e3e2e9
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-19 00:06:24 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
 import { Profile } from './01-profile/Profile'
 import user from '../data/user'
-import Statistics from './02-statistics/Statistics'
+import { Statistics } from './02-statistics/Statistics'
 import data from '../data/data'
-import FriendList from './03-friendList/FriendList'
+import { FriendList } from './03-friendList/FriendList'
 import friends from '../data/friends'
-import TransactionHistory from './04-transactionHistory/TransactionHistory'
+import { TransactionHistory } from './04-transactionHistory/TransactionHistory'
 import transactions from '../data/transactions'
-
+import { Box } from "./Box";
 
 export const App = () => {
   return (
-    <>
+    <Box
+      bg='background'
+      color='text'
+      display='grid'
+      alignItems='center'
+      fontSize={14}
+      pt={4}
+      pb={4}
+    as='main'>
       <Profile
         username={user.username}
         tag={user.tag}
@@ -30,6 +38,6 @@ export const App = () => {
       <TransactionHistory 
         items={transactions}
       />
-    </>
+    </Box>
   )
 };
