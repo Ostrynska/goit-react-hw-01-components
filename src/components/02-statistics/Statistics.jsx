92cf948388566d60e0ce32ff737d134b1f53a9be
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-22 15:29:27 +0200

    add index.js

--- src/components/02-statistics/Statistics.jsx
+++ src/components/02-statistics/Statistics.jsx
@@ -1,37 +1,54 @@
 import PropTypes from 'prop-types';
-import { StatisticSection, StatisticTitle, StatisticList, StatisticItem, StatisticLabel, StatisticPercentage } from "./Statistics.styled";
+import {
+ StatisticSection,
+ StatisticTitle,
+ StatisticList,
+ StatisticItem,
+ StatisticLabel,
+ StatisticPercentage,
+} from './Statistics.styled';
 
-export const  Statistics = ({ title, stats }) => {
-  return (
-    <StatisticSection>
-      {title && <StatisticTitle>{title}</StatisticTitle>}
+export const Statistics = ({ title, stats }) => {
+ return (
+  <StatisticSection>
+   {title && <StatisticTitle>{title}</StatisticTitle>}
 
-      <StatisticList>
-        {stats.map(({ id, label, percentage }) => {
-          return (
-            <StatisticItem key={id} style={{ backgroundColor: changeHex() }}>
-              <StatisticLabel as='p'>{label}</StatisticLabel>
-              <StatisticPercentage as='p'>{percentage}%</StatisticPercentage>
-            </StatisticItem>
-          );
-        })}
-      </StatisticList>
-    </StatisticSection>
-  )
+   <StatisticList>
+    {stats.map(({ id, label, percentage }) => {
+     return (
+      <StatisticItem key={id} style={{ backgroundColor: changeHex() }}>
+       <StatisticLabel as="p">{label}</StatisticLabel>
+       <StatisticPercentage as="p">{percentage}%</StatisticPercentage>
+      </StatisticItem>
+     );
+    })}
+   </StatisticList>
+  </StatisticSection>
+ );
 };
 
-function changeHex()
-{
-  const hex = ['#F3B848', '#bd8811','#ffea79 ', '#ffcdd2', '#ff94c2', '#f06292', '#76435e', '#9c81f2', '#D85841'];
-  return hex[Math.floor(Math.random() * hex.length)]
+function changeHex() {
+ const hex = [
+  '#F3B848',
+  '#bd8811',
+  '#ffea79 ',
+  '#ffcdd2',
+  '#ff94c2',
+  '#f06292',
+  '#76435e',
+  '#9c81f2',
+  '#D85841',
+ ];
+ return hex[Math.floor(Math.random() * hex.length)];
 }
 
 Statistics.propTypes = {
-    title: PropTypes.string.isRequired,
-    stats: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
-        percentage: PropTypes.number.isRequired,
-    })
-    ).isRequired
-}
+ title: PropTypes.string.isRequired,
+ stats: PropTypes.arrayOf(
+  PropTypes.shape({
+   id: PropTypes.string.isRequired,
+   label: PropTypes.string.isRequired,
+   percentage: PropTypes.number.isRequired,
+  })
+ ).isRequired,
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,36 @@
-import { Profile } from './01-profile/Profile'
-import user from '../data/user'
-import { Statistics } from './02-statistics/Statistics'
-import data from '../data/data'
-import { FriendList } from './03-friendList/FriendList'
-import friends from '../data/friends'
-import { TransactionHistory } from './04-transactionHistory/TransactionHistory'
-import transactions from '../data/transactions'
-import { Box } from "./Box";
+import { Box } from './Box';
+import { Profile } from './01-profile';
+import { Statistics } from './02-statistics';
+import { FriendList } from './03-friendList';
+import { TransactionHistory } from './04-transactionHistory';
+
+import user from '../data/user';
+import data from '../data/data';
+import friends from '../data/friends';
+import transactions from '../data/transactions';
 
 export const App = () => {
-  return (
-    <Box
-      bg='background'
-      color='text'
-      display='grid'
-      alignItems='center'
-      fontSize={14}
-      pt={4}
-      pb={4}
-    as='main'>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-    />
-      <Statistics 
-        title="Upload stats"  
-        stats={data}
-      />
-      <FriendList 
-        friends={friends}
-      />
-      <TransactionHistory 
-        items={transactions}
-      />
-    </Box>
-  )
+ return (
+  <Box
+   bg="background"
+   color="text"
+   display="grid"
+   alignItems="center"
+   fontSize={14}
+   pt={4}
+   pb={4}
+   as="main"
+  >
+   <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+   />
+   <Statistics title="Upload stats" stats={data} />
+   <FriendList friends={friends} />
+   <TransactionHistory items={transactions} />
+  </Box>
+ );
 };
